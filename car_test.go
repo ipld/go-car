32d0d993382360d0dc14d86325636df2154081c4
Łukasz Magiera <magik6k@gmail.com>
2020-05-26 15:33:14 +0200

    Handle reading null-padded files

--- car_test.go
+++ car_test.go
@@ -84,6 +84,46 @@ func TestRoundtrip(t *testing.T) {
 	}
 }
 
+func TestRoundtripNullPadded(t *testing.T) {
+	dserv := dstest.Mock()
+	a := merkledag.NewRawNode([]byte("aaaa"))
+
+	assertAddNodes(t, dserv, a)
+
+	buf := new(bytes.Buffer)
+	if err := car.WriteCar(context.Background(), dserv, []cid.Cid{a.Cid()}, buf); err != nil {
+		t.Fatal(err)
+	}
+
+	buf.Write([]byte{0, 0, 0, 0, 0})
+
+	bserv := dstest.Bserv()
+	ch, err := car.LoadCar(bserv.Blockstore(), buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(ch.Roots) != 1 {
+		t.Fatal("should have one root")
+	}
+
+	if !ch.Roots[0].Equals(a.Cid()) {
+		t.Fatal("got wrong cid")
+	}
+
+	bs := bserv.Blockstore()
+	for _, nd := range []format.Node{a} {
+		has, err := bs.Has(nd.Cid())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !has {
+			t.Fatal("should have cid in blockstore")
+		}
+	}
+}
+
 func TestRoundtripFilestore(t *testing.T) {
 	dserv := dstest.Mock()
 	a := merkledag.NewRawNode([]byte("aaaa"))
