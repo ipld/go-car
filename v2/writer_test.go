1f8bfaa25241acf89b8ed2e25b7c99176ef95975
Masih H. Derkani <m@derkani.org>
2021-06-10 16:59:26 +0100

    Implement CAR v2 writer that uses `format.NodeGetter`
    Implement a CAR v2 writer, that produces binary structure corresponding
    to the specification of car V2, consisting of:
     1. Version prefix
     2. CAR v2 header
     3. Dump of Car v1
     4. Carbs index
    
    The implementation also facilities optional padding before dump of CAR
    v1 and Carbs index to provide scope for future optimisations. The
    padding is defined as a dedicated type that writes zero-valued bytes for
    a given padding size. The CAR v2 header, then captures the offsets from
    the beginning of the file for both the CAR v1 dump and Carbs index. This
    allows the user to quickly skip to the part of the CAR v2 they need as
    well as offset alignment, if necessary, by altering the value of
    padding.
    
    Note, this is an intermediate implementation, and does not correctly
    count the number of bytes written by the writer, pending the
    transfer/refactor of Carbs implementation in this repo. In the meantime,
    This implementation depends on Carbs as a go module. The implementation
    of extensive writer tests is postponed until Carbs is transferred and
    the written byte-count can be returned from the index marshaller.

---
+++ v2/writer_test.go
@@ -0,0 +1,89 @@
+package car
+
+import (
+	"bytes"
+	"context"
+	"github.com/ipfs/go-cid"
+	format "github.com/ipfs/go-ipld-format"
+	dag "github.com/ipfs/go-merkledag"
+	dstest "github.com/ipfs/go-merkledag/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/willscott/carbs"
+	"testing"
+)
+
+func TestPadding_WriteTo(t *testing.T) {
+	tests := []struct {
+		name      string
+		padding   Padding
+		wantBytes []byte
+		wantN     int64
+		wantErr   bool
+	}{
+		{
+			"zero padding is no bytes",
+			Padding(0),
+			nil,
+			0,
+			false,
+		},
+		{
+			"non-zero padding is corresponding zero-value bytes",
+			Padding(3),
+			[]byte{0x00, 0x00, 0x00},
+			3,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := &bytes.Buffer{}
+			gotN, gotErr := tt.padding.WriteTo(w)
+			if tt.wantErr {
+				assert.Error(t, gotErr)
+				return
+			}
+			gotBytes := w.Bytes()
+			assert.Equal(t, tt.wantN, gotN)
+			assert.Equal(t, tt.wantBytes, gotBytes)
+		})
+	}
+}
+
+func TestNewWriter(t *testing.T) {
+	dagService := dstest.Mock()
+	wantRoots := generateRootCid(t, dagService)
+	writer := NewWriter(context.Background(), dagService, wantRoots)
+	assert.Equal(t, carbs.IndexSorted, writer.IndexCodec)
+	assert.Equal(t, wantRoots, writer.roots)
+}
+
+func generateRootCid(t *testing.T, adder format.NodeAdder) []cid.Cid {
+	// TODO convert this into a utility testing lib that takes an rng and generates a random DAG with some threshold for depth/breadth.
+	this := dag.NewRawNode([]byte("fish"))
+	that := dag.NewRawNode([]byte("lobster"))
+	other := dag.NewRawNode([]byte("🌊"))
+
+	one := &dag.ProtoNode{}
+	assertAddNodeLink(t, one, this, "fishmonger")
+
+	another := &dag.ProtoNode{}
+	assertAddNodeLink(t, another, one, "barreleye")
+	assertAddNodeLink(t, another, that, "🐡")
+
+	andAnother := &dag.ProtoNode{}
+	assertAddNodeLink(t, andAnother, another, "🍤")
+
+	assertAddNodes(t, adder, this, that, other, one, another, andAnother)
+	return []cid.Cid{andAnother.Cid()}
+}
+
+func assertAddNodeLink(t *testing.T, pn *dag.ProtoNode, fn format.Node, name string) {
+	assert.NoError(t, pn.AddNodeLink(name, fn))
+}
+
+func assertAddNodes(t *testing.T, adder format.NodeAdder, nds ...format.Node) {
+	for _, nd := range nds {
+		assert.NoError(t, adder.Add(context.Background(), nd))
+	}
+}
