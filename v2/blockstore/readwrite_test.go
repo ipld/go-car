895bbea0c546596d169d160977ab9e797e7056ac
aarshkshah1992 <aarshkshah1992@gmail.com>
2021-06-29 16:08:13 +0530

    traversal fails

--- v2/blockstore/readwrite_test.go
+++ v2/blockstore/readwrite_test.go
@@ -8,15 +8,30 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ipfs/go-blockservice"
+	ds "github.com/ipfs/go-datastore"
+	bstore "github.com/ipfs/go-ipfs-blockstore"
+	chunk "github.com/ipfs/go-ipfs-chunker"
+	offline "github.com/ipfs/go-ipfs-exchange-offline"
+	ipldformat "github.com/ipfs/go-ipld-format"
+	"github.com/ipfs/go-merkledag"
+	"github.com/ipfs/go-unixfs/importer/balanced"
+	"github.com/ipfs/go-unixfs/importer/helpers"
+	"github.com/ipld/go-car"
+	"github.com/ipld/go-car/v2/blockstore"
+	basicnode "github.com/ipld/go-ipld-prime/node/basic"
+	"github.com/ipld/go-ipld-prime/traversal/selector"
+	"github.com/ipld/go-ipld-prime/traversal/selector/builder"
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/require"
-
-	"github.com/ipld/go-car/v2/blockstore"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	"github.com/ipld/go-car/v2/internal/carv1"
 )
+
+const unixfsLinksPerLevel = 1024
+const unixfsChunkSize uint64 = 1 << 10
 
 func TestBlockstore(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
@@ -166,4 +181,93 @@ func TestBlockstorePutSameHashes(t *testing.T) {
 
 	err = wbs.Finalize()
 	require.NoError(t, err)
+}
+
+func TestUnixFSDAGcreation(t *testing.T) {
+	ctx := context.Background()
+
+	// create a Unix FS DAG using a map based blockstore.
+	bs := bstore.NewBlockstore(ds.NewMapDatastore())
+	dag := merkledag.NewDAGService(blockservice.New(bs, offline.Exchange(bs)))
+
+	// import to UnixFS
+	bufferedDS := ipldformat.NewBufferedDAG(ctx, dag)
+
+	params := helpers.DagBuilderParams{
+		Maxlinks:   unixfsLinksPerLevel,
+		RawLeaves:  true,
+		CidBuilder: nil,
+		Dagserv:    bufferedDS,
+	}
+
+	f, err := os.Open("testdata/payload.txt")
+	require.NoError(t, err)
+	defer f.Close()
+
+	db, err := params.New(chunk.NewSizeSplitter(f, int64(unixfsChunkSize)))
+	require.NoError(t, err)
+
+	nd, err := balanced.Layout(db)
+	require.NoError(t, err)
+
+	err = bufferedDS.Commit()
+	require.NoError(t, err)
+
+	require.NoError(t, f.Close())
+
+	// ----- Now that we have the root, generate the same Unix FS DAG Again with a CARv2 read-write blockstore.
+	carV2Path := genWithCARv2Blockstore(t, nd.Cid())
+
+	rdOnly, err := blockstore.OpenReadOnly(carV2Path, true)
+	require.NoError(t, err)
+	defer rdOnly.Close()
+
+	// do a CARv1 traversal with the DFS selector on the CARv2 generated above.
+	ssb := builder.NewSelectorSpecBuilder(basicnode.Prototype.Any)
+	allSelector := ssb.ExploreRecursive(selector.RecursionLimitNone(),
+		ssb.ExploreAll(ssb.ExploreRecursiveEdge())).Node()
+
+	sc := car.NewSelectiveCar(ctx, rdOnly, []car.Dag{{Root: nd.Cid(), Selector: allSelector}})
+	_, err = sc.Prepare()
+	require.NoError(t, err)
+}
+
+func genWithCARv2Blockstore(t *testing.T, root cid.Cid) string {
+	ctx := context.Background()
+
+	tmp, err := os.CreateTemp("", "rand")
+	require.NoError(t, err)
+	require.NoError(t, tmp.Close())
+
+	rw, err := blockstore.NewReadWrite(tmp.Name(), []cid.Cid{root})
+	require.NoError(t, err)
+
+	bsvc := blockservice.New(rw, offline.Exchange(rw))
+	dag := merkledag.NewDAGService(bsvc)
+	// import to UnixFS
+	bufferedDS := ipldformat.NewBufferedDAG(ctx, dag)
+
+	params := helpers.DagBuilderParams{
+		Maxlinks:   unixfsLinksPerLevel,
+		RawLeaves:  true,
+		CidBuilder: nil,
+		Dagserv:    bufferedDS,
+	}
+
+	f, err := os.Open("testdata/payload.txt")
+	require.NoError(t, err)
+
+	db, err := params.New(chunk.NewSizeSplitter(f, int64(unixfsChunkSize)))
+	require.NoError(t, err)
+
+	// TODO: The below lines fail with "not found".
+	_, err = balanced.Layout(db)
+	require.NoError(t, err)
+
+	err = bufferedDS.Commit()
+	require.NoError(t, err)
+
+	require.NoError(t, rw.Finalize())
+
+	return tmp.Name()
 }
