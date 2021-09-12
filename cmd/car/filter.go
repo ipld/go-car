2701d8df652b33540d27c09fc16066e33c058410
Will Scott <will@cypherpunk.email>
2021-09-12 05:45:18 -0700

    update cli to separate module

--- cmd/car/filter.go
+++ cmd/car/filter.go
@@ -10,7 +10,7 @@ import (
 	"github.com/ipfs/go-cid"
 	carv2 "github.com/ipld/go-car/v2"
 	"github.com/ipld/go-car/v2/blockstore"
-	icarv1 "github.com/ipld/go-car/v2/internal/carv1"
+	carv1 "github.com/ipld/go-car"
 	"github.com/urfave/cli/v2"
 )
 
@@ -49,7 +49,7 @@ func FilterCar(c *cli.Context) error {
 	}
 	fmt.Printf("filtering to %d cids\n", len(cidMap))
 
-	rd, err := icarv1.NewCarReader(r.DataReader())
+	rd, err := carv1.NewCarReader(r.DataReader())
 	if err != nil {
 		return err
 	}
