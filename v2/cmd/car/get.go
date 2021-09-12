7112b66ad87cf7207a7009a7d4b82b8296e91333
Will Scott <will@cypherpunk.email>
2021-09-12 05:16:48 -0700

    better output printing

--- v2/cmd/car/get.go
+++ v2/cmd/car/get.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ipfs/go-cid"
 	"github.com/ipld/go-car/v2/blockstore"
@@ -11,7 +12,7 @@ import (
 // GetCarBlock is a command to get a block out of a car
 func GetCarBlock(c *cli.Context) error {
 	if c.Args().Len() < 2 {
-		return fmt.Errorf("usage: car get-block <file.car> <block cid>")
+		return fmt.Errorf("usage: car get-block <file.car> <block cid> [output file]")
 	}
 
 	bs, err := blockstore.OpenReadOnly(c.Args().Get(0))
@@ -29,6 +30,16 @@ func GetCarBlock(c *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf(string(blk.RawData()))
-	return nil
+
+	outStream := os.Stdout
+	if c.Args().Len() >= 3 {
+		outStream, err = os.Create(c.Args().Get(2))
+		if err != nil {
+			return err
+		}
+		defer outStream.Close()
+	}
+
+	_, err := outStream.Write(blk.RawData())
+	return err
 }
