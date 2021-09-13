367df99a96a49c9b139613e1a27d540225daac1a
Aarsh Shah <aarshkshah1992@gmail.com>
2021-09-13 16:58:18 +0400

    failing test

--- v2/example_test.go
+++ v2/example_test.go
@@ -2,15 +2,42 @@ package car_test
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/ipfs/go-cidutil"
+	bstore "github.com/ipfs/go-ipfs-blockstore"
+	chunker "github.com/ipfs/go-ipfs-chunker"
+	ipld "github.com/ipfs/go-ipld-format"
+	"github.com/ipfs/go-unixfs/importer/balanced"
+	ihelper "github.com/ipfs/go-unixfs/importer/helpers"
+	mh "github.com/multiformats/go-multihash"
+
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-fil-markets/stores"
+	"github.com/ipfs/go-blockservice"
+	"github.com/ipfs/go-cid"
+	offline "github.com/ipfs/go-ipfs-exchange-offline"
+	files "github.com/ipfs/go-ipfs-files"
+	"github.com/ipfs/go-merkledag"
+	unixfile "github.com/ipfs/go-unixfs/file"
+	"github.com/stretchr/testify/require"
 
 	carv2 "github.com/ipld/go-car/v2"
 	"github.com/ipld/go-car/v2/blockstore"
 )
+
+var DefaultHashFunction = uint64(mh.BLAKE2B_MIN + 31)
+
+const UnixfsChunkSize uint64 = 1 << 20
+const UnixfsLinksPerLevel = 1024
 
 func ExampleWrapV1File() {
 	// We have a sample CARv1 file.
@@ -126,4 +153,183 @@ func ExampleNewBlockReader() {
 	// 	bafy2bzaceb62wdepofqu34afqhbcn4a7jziwblt2ih5hhqqm6zitd3qpzhdp4
 	// 	bafy2bzaceb3utcspm5jqcdqpih3ztbaztv7yunzkiyfq7up7xmokpxemwgu5u
 	// 	bafy2bzacedjwekyjresrwjqj4n2r5bnuuu3klncgjo2r3slsp6wgqb37sz4ck
+}
+
+func TestFail(t *testing.T) {
+	ctx := context.Background()
+
+	inputPath, _ := genInputFile(t)
+	defer os.Remove(inputPath) //nolint:errcheck
+
+	dst := newTmpFile(t)
+	defer os.Remove(dst) //nolint:errcheck
+
+	root, err := createUnixFSFilestore(ctx, inputPath, dst)
+	require.NoError(t, err)
+	require.NotEqual(t, cid.Undef, root)
+
+	// convert the CARv2 to a normal file again and ensure the contents match
+	fs, err := stores.ReadOnlyFilestore(dst)
+	require.NoError(t, err)
+	defer fs.Close() //nolint:errcheck
+
+	dags := merkledag.NewDAGService(blockservice.New(fs, offline.Exchange(fs)))
+
+	nd, err := dags.Get(ctx, root)
+	require.NoError(t, err)
+
+	file, err := unixfile.NewUnixfsFile(ctx, dags, nd)
+	require.NoError(t, err)
+
+	tmpOutput := newTmpFile(t)
+	defer os.Remove(tmpOutput) //nolint:errcheck
+	require.NoError(t, files.WriteTo(file, tmpOutput))
+}
+
+// createUnixFSFilestore takes a standard file whose path is src, forms a UnixFS DAG, and
+// writes a CARv2 file with positional mapping (backed by the go-filestore library).
+func createUnixFSFilestore(ctx context.Context, srcPath string, dstPath string) (cid.Cid, error) {
+	// This method uses a two-phase approach with a staging CAR blockstore and
+	// a final CAR blockstore.
+	//
+	// This is necessary because of https://github.com/ipld/go-car/issues/196
+	//
+	// TODO: do we need to chunk twice? Isn't the first output already in the
+	//  right order? Can't we just copy the CAR file and replace the header?
+
+	src, err := os.Open(srcPath)
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("failed to open input file: %w", err)
+	}
+	defer src.Close() //nolint:errcheck
+
+	stat, err := src.Stat()
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("failed to stat file :%w", err)
+	}
+
+	file, err := files.NewReaderPathFile(srcPath, src, stat)
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("failed to create reader path file: %w", err)
+	}
+
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("failed to create temp file: %w", err)
+	}
+	_ = f.Close() // close; we only want the path.
+
+	tmp := f.Name()
+	defer os.Remove(tmp) //nolint:errcheck
+
+	// Step 1. Compute the UnixFS DAG and write it to a CARv2 file to get
+	// the root CID of the DAG.
+	fstore, err := stores.ReadWriteFilestore(tmp)
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("failed to create temporary filestore: %w", err)
+	}
+
+	finalRoot1, err := buildUnixFS(ctx, file, fstore, true)
+	if err != nil {
+		_ = fstore.Close()
+		return cid.Undef, xerrors.Errorf("failed to import file to store to compute root: %w", err)
+	}
+
+	if err := fstore.Close(); err != nil {
+		return cid.Undef, xerrors.Errorf("failed to finalize car filestore: %w", err)
+	}
+
+	// Step 2. We now have the root of the UnixFS DAG, and we can write the
+	// final CAR for real under `dst`.
+	bs, err := stores.ReadWriteFilestore(dstPath, finalRoot1)
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("failed to create a carv2 read/write filestore: %w", err)
+	}
+
+	// rewind file to the beginning.
+	if _, err := src.Seek(0, 0); err != nil {
+		return cid.Undef, xerrors.Errorf("failed to rewind file: %w", err)
+	}
+
+	finalRoot2, err := buildUnixFS(ctx, file, bs, true)
+	if err != nil {
+		_ = bs.Close()
+		return cid.Undef, xerrors.Errorf("failed to create UnixFS DAG with carv2 blockstore: %w", err)
+	}
+
+	if err := bs.Close(); err != nil {
+		return cid.Undef, xerrors.Errorf("failed to finalize car blockstore: %w", err)
+	}
+
+	if finalRoot1 != finalRoot2 {
+		return cid.Undef, xerrors.New("roots do not match")
+	}
+
+	return finalRoot1, nil
+}
+
+func newTmpFile(t *testing.T) string {
+	f, err := os.CreateTemp("", "")
+	require.NoError(t, err)
+	require.NoError(t, f.Close())
+	return f.Name()
+}
+
+func genInputFile(t *testing.T) (filepath string, contents []byte) {
+	s := strings.Repeat("abcde", 100)
+	tmp, err := os.CreateTemp("", "")
+	require.NoError(t, err)
+	_, err = io.Copy(tmp, strings.NewReader(s))
+	require.NoError(t, err)
+	require.NoError(t, tmp.Close())
+	return tmp.Name(), []byte(s)
+}
+
+// buildUnixFS builds a UnixFS DAG out of the supplied reader,
+// and imports the DAG into the supplied service.
+func buildUnixFS(ctx context.Context, reader io.Reader, into bstore.Blockstore, filestore bool) (cid.Cid, error) {
+	b, err := unixFSCidBuilder()
+	if err != nil {
+		return cid.Undef, err
+	}
+
+	bsvc := blockservice.New(into, offline.Exchange(into))
+	dags := merkledag.NewDAGService(bsvc)
+	bufdag := ipld.NewBufferedDAG(ctx, dags)
+
+	params := ihelper.DagBuilderParams{
+		Maxlinks:   UnixfsLinksPerLevel,
+		RawLeaves:  true,
+		CidBuilder: b,
+		Dagserv:    bufdag,
+		NoCopy:     filestore,
+	}
+
+	db, err := params.New(chunker.NewSizeSplitter(reader, int64(UnixfsChunkSize)))
+	if err != nil {
+		return cid.Undef, err
+	}
+	nd, err := balanced.Layout(db)
+	if err != nil {
+		return cid.Undef, err
+	}
+
+	if err := bufdag.Commit(); err != nil {
+		return cid.Undef, err
+	}
+
+	return nd.Cid(), nil
+}
+
+func unixFSCidBuilder() (cid.Builder, error) {
+	prefix, err := merkledag.PrefixForCidVersion(1)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize UnixFS CID Builder: %w", err)
+	}
+	prefix.MhType = DefaultHashFunction
+	b := cidutil.InlineBuilder{
+		Builder: prefix,
+		Limit:   126,
+	}
+	return b, nil
 }
--- v2/go.mod
+++ v2/go.mod
@@ -3,24 +3,26 @@ module github.com/ipld/go-car/v2
 go 1.16
 
 require (
+	github.com/filecoin-project/go-fil-markets v1.12.0
 	github.com/ipfs/go-block-format v0.0.3
+	github.com/ipfs/go-blockservice v0.1.5
 	github.com/ipfs/go-cid v0.0.8-0.20210716091050-de6c03deae1c
-	github.com/ipfs/go-ipfs-blockstore v1.0.3
+	github.com/ipfs/go-cidutil v0.0.2
+	github.com/ipfs/go-ipfs-blockstore v1.0.4
+	github.com/ipfs/go-ipfs-chunker v0.0.5
+	github.com/ipfs/go-ipfs-exchange-offline v0.0.1
+	github.com/ipfs/go-ipfs-files v0.0.8
 	github.com/ipfs/go-ipld-cbor v0.0.5
 	github.com/ipfs/go-ipld-format v0.2.0
 	github.com/ipfs/go-merkledag v0.3.2
-	github.com/klauspost/cpuid/v2 v2.0.8 // indirect
-	github.com/kr/pretty v0.2.1 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/ipfs/go-unixfs v0.2.6
+	github.com/ipld/go-car v0.3.1
 	github.com/multiformats/go-multicodec v0.3.1-0.20210902112759-1539a079fd61
 	github.com/multiformats/go-multihash v0.0.15
 	github.com/multiformats/go-varint v0.0.6
 	github.com/petar/GoLLRB v0.0.0-20210522233825-ae3b015fd3e9
-	github.com/polydawn/refmt v0.0.0-20201211092308-30ac6d18308e // indirect
 	github.com/stretchr/testify v1.7.0
-	github.com/warpfork/go-wish v0.0.0-20200122115046-b9ea61034e4a // indirect
 	github.com/whyrusleeping/cbor v0.0.0-20171005072247-63513f603b11
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/exp v0.0.0-20210615023648-acb5c1269671
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/exp v0.0.0-20210715201039-d37aa40e8013
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 )
