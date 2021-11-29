0b392bc59b3c88f32b6a57a9f9aed28a33f58859
Will Scott <will.scott@protocol.ai>
2021-11-29 15:19:34 -0800

    additional testing as requested

--- v2/selective_test.go
+++ v2/selective_test.go
@@ -37,6 +37,13 @@ func TestPrepareTraversal(t *testing.T) {
 
 	fi, _ := os.Stat("testdata/sample-unixfs-v2.car")
 	require.Equal(t, fi.Size(), n)
+
+	// Headers should be equal
+	h1, _ := car.OpenReader("testdata/sample-unixfs-v2.car")
+	h1h := bytes.Buffer{}
+	h1h.Write(car.Pragma)
+	h1.Header.WriteTo(&h1h)
+	require.Equal(t, buf.Bytes()[:h1h.Len()], h1h.Bytes())
 }
 
 func TestFileTraversal(t *testing.T) {
@@ -56,4 +63,21 @@ func TestFileTraversal(t *testing.T) {
 	fa, _ := os.Stat("testdata/sample-unixfs-v2.car")
 	fb, _ := os.Stat(path.Join(outDir, "out.car"))
 	require.Equal(t, fa.Size(), fb.Size())
+}
+
+func TestV1Traversal(t *testing.T) {
+	from, err := blockstore.OpenReadOnly("testdata/sample-v1.car")
+	require.NoError(t, err)
+	ls := cidlink.DefaultLinkSystem()
+	bsa := bsadapter.Adapter{Wrapped: from}
+	ls.SetReadStorage(&bsa)
+
+	rts, _ := from.Roots()
+	w := bytes.NewBuffer(nil)
+	n, err := car.TraverseV1(context.Background(), &ls, rts[0], selectorparse.CommonSelector_ExploreAllRecursively, w)
+	require.NoError(t, err)
+	require.Equal(t, int64(len(w.Bytes())), int64(n))
+
+	fa, _ := os.Stat("testdata/sample-v1.car")
+	require.Equal(t, fa.Size(), int64(n))
 }
